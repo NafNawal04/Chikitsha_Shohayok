84cd12f56949dc6bea5a2dffe2c597ef74382a06
Nafisa Nawal <nafisanawal@iut-dhaka.edu>
2025-05-06 06:42:55 +0600

    deploy

--- app.js
+++ app.js
@@ -180,7 +180,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'landingPage.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`App is listening to port ${port}`);
 });
